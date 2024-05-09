11060f8b208ac490255df42845cb7ba9f7d4a011
Danny Chen <dannychen@Dannys-Air.lan1>
2024-05-09 18:11:54 -0400

    feat: changeQty() func added to update subtotal afterchanging desired qty refactor: func renames and restructured how updateSubtotal() works

--- static/cart.js
+++ static/cart.js
@@ -16,7 +16,40 @@ function deleteCartItem() {
     }
 }
 
-function updateTotalItem() {
+function changeQty() {
+    // Changes subtotal after changing qty
+    let qtySelect = document.querySelectorAll(".qty");
+    let cartPrices = document.querySelectorAll(".cart__item-price");
+    let prevQty;
+
+    for (let i = 0; i < qtySelect.length; i++) {
+        // To save previous qty
+        qtySelect[i].addEventListener("click", function() {
+            // console.log(qtySelect[i].value);
+            prevQty = qtySelect[i].value;
+        });
+
+        qtySelect[i].addEventListener("change", function(e) {
+            let subtotal = document.querySelector(".cart__subtotal-amt");
+            let newQty = e.target.value;
+            // console.log(newQty, prevQty);
+            
+            let operation = newQty > prevQty ? "+" : "-";
+            let qtyDiff = newQty > prevQty ? newQty - prevQty : prevQty - newQty;
+            
+            let newSubtotal = updateSubtotal(
+                currencyToNum(subtotal.innerText),
+                currencyToNum(cartPrices[i].innerText),
+                qtyDiff,
+                operation
+            );
+            
+            subtotal.innerText = "$" + newSubtotal.toFixed(2);
+        }); 
+    }
+}
+
+function updateCartTotalItem() {
     let qtys = document.querySelectorAll(".desired-qty");
     let totalItem = document.querySelector(".cart__total-items");
         
@@ -28,13 +61,25 @@ function updateTotalItem() {
     totalItem.innerText = `(${itemCount} items)`;
 }
 
-function updateSubtotal(pricesList, qtysList) {
+function updateCartSubtotal() {
+    let cartPrices = document.querySelectorAll(".cart__item-price");
+    let qtys = document.querySelectorAll(".desired-qty");
+
     let newSubtotal = 0;
-    for (let i = 0; i < pricesList.length; i++) {
-        newSubtotal = newSubtotal + (currencyToNum(pricesList[i].innerText) * parseInt(qtysList[i].innerText));
+    for (let i = 0; i < cartPrices.length; i++) {
+        newSubtotal = updateSubtotal(newSubtotal, currencyToNum(cartPrices[i].innerText), parseInt(qtys[i].innerText))
     }
 
     return "$" + newSubtotal.toFixed(2);
+}
+
+function updateSubtotal(currentSubtotal, price, qty, operation="+") {
+    if (operation == "+") {
+        return currentSubtotal + (parseInt(price) * parseInt(qty));
+    }
+    else if (operation == "-") {
+        return currentSubtotal - (parseInt(price) * parseInt(qty));
+    }
 }
 
 function deleteChild(ele) {
@@ -43,21 +88,15 @@ function deleteChild(ele) {
     ele.textContent = "";
 }
 
-
 // Sockets
 socket.on("delete cart item", function(index) {
     let cartItems = document.querySelectorAll(".cart__item");
     let subtotal = document.querySelector(".cart__subtotal-amt");
 
     deleteChild(cartItems[index]);
-
-    // Get prices AFTER undesired item is deleted
-    let cartPrices = document.querySelectorAll(".cart__item-price");
-    let qtys = document.querySelectorAll(".desired-qty");
-    subtotal.innerText = updateSubtotal(cartPrices, qtys)
-
-    // Update total item count
-    updateTotalItem();
+    
+    subtotal.innerText = updateCartSubtotal();
+    updateCartTotalItem();
 
     // Reload the page once the cart reaches 0 items
     if (subtotal.innerText == "$0.00") {
@@ -66,3 +105,4 @@ socket.on("delete cart item", function(index) {
 });
 
 deleteCartItem();
+changeQty();
